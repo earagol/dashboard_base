709d94327f1b2373b821adece8ddf4c046f8d718
earagol <aragoll@andain.cl>
2019-06-06 10:58:14 -0400

    1

--- config/routes.php
+++ config/routes.php
@@ -78,3 +78,11 @@ Router::scope('/', function (RouteBuilder $routes) {
      */
     $routes->fallbacks(DashedRoute::class);
 });
+
+Router::prefix('api', function ($routes) {
+    $routes->extensions(['json', 'xml']);
+    $routes->resources('Cocktails');
+    $routes->resources('Users');
+    Router::connect('/api/users/register', ['controller' => 'Users', 'action' => 'add', 'prefix' => 'api']);
+    $routes->fallbacks('InflectedRoute');
+});
